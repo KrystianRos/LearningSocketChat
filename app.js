eb66bd1bd523467d73f5a5d933ac1925014004c7
Kry R <ros.kry@gmail.com>
2019-01-12 17:41:12 +0100

    Simple chat finished

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-'use strict';
-
 const app = require('express')();
 const http = require('http').Server(app);
 const path = require('path');
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+(function () {
+  'use strict';
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  io.on('connect', (socket)=>{
+    console.log('A user has been conencted');
+    socket.on('chat message', (data) => {
+      console.log(`message ${data} received`);
+      io.emit('chat message', data);
+    });
+  });
+
+  http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+}());
